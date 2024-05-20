0de49cc161a842af14169564a263cfba2ef398a3
Romain DI CANDIDO <116581960+RomainDC73@users.noreply.github.com>
2024-05-20 10:23:24 +0200

    event listener on return key

--- js/form.js
+++ js/form.js
@@ -15,16 +15,33 @@ window.onload = () => {
   resultElement.textContent = '';
 };
 
-// Événement de clic sur le bouton OK
-okButton.addEventListener('click', () => {
+// Fonction pour valider le champ de saisie
+function validateInput() {
     const inputValue = numberInput.value.trim();
-
+  
+    // Vérifier si la valeur est un nombre entier entre 0 et 20
     if (isValidNumber(inputValue)) {
-      resultElement.textContent = 'Vous avez entré le nombre : '+ inputValue;
+      resultElement.textContent = `Vous avez entré le nombre : ${inputValue}`;
     } else {
-      resultElement.textContent = 'Vous devez entrer un nombre entre 0 et 20.';
+      resultElement.textContent = 'Veuillez entrer un nombre entier entre 0 et 20.';
     }
+  
+    // Vider le champ de saisie après validation
+    numberInput.value = '';
+  }
+  
+    // Ajouter un écouteur d'événement pour le bouton OK
+    okButton.addEventListener('click', validateInput);
 
+    // Ajouter un écouteur d'événement pour la touche "Entrée"
+    numberInput.addEventListener('keydown', function(event) {
+        if (event.key === 'Enter') {
+        validateInput();
+    }
+});
+
+// Vider le champ de saisie lors du chargement de la page
+window.addEventListener('load', function() {
     numberInput.value = '';
 });
 
