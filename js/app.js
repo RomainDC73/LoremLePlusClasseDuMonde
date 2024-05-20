909d89753644b47ea182d083d442fbfbc6b044a5
Romain DI CANDIDO <116581960+RomainDC73@users.noreply.github.com>
2024-05-20 11:25:25 +0200

    shuffled quotes

--- js/app.js
+++ js/app.js
@@ -1,5 +1,14 @@
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  }
+
 function displayQuotes(n) {
-    const quotesToDisplay = quotes.slice(0, n);
+    const shuffledQuotes = shuffleArray(quotes.slice());
+    const quotesToDisplay = shuffledQuotes.slice(0, n);
     resultElement.innerHTML = quotesToDisplay.join('\n');
 };
 
