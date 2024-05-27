8758f4146c956b01e765646f4b36b9f9580f9341
Romain DI CANDIDO <116581960+RomainDC73@users.noreply.github.com>
2024-05-27 10:30:14 +0200

    copy button

---
+++ js/copy.js
@@ -0,0 +1,26 @@
+const quotesToCopy = document.getElementById('result');
+const copyButton = document.getElementById('copyButton');
+
+copyButton.addEventListener('click', () => {
+  const textToCopy = quotesToCopy.textContent.trim();
+
+  if (navigator.clipboard) {
+    // Utiliser l'API Clipboard pour copier le texte
+    navigator.clipboard.writeText(textToCopy)
+      .then(() => {
+        console.log('Texte copié !');
+      })
+      .catch((err) => {
+        console.error('Échec de la copie du texte: ', err);
+      });
+  } else {
+    // Utiliser la méthode de secours pour les navigateurs plus anciens
+    const tempInput = document.createElement('textarea');
+    tempInput.value = textToCopy;
+    document.body.appendChild(tempInput);
+    tempInput.select();
+    document.execCommand('copy');
+    document.body.removeChild(tempInput);
+    console.log('Texte copié dans le presse-papiers');
+  }
+});
